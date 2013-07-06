7dc60fe3a3ab0c3550900f3d7c526f98483535ca
Duncan Bayne <dhgbayne@gmail.com>
2013-07-06 17:44:24 +1000

    Now handles nested lists.

--- src/cons_cell.c
+++ src/cons_cell.c
@@ -1,7 +1,7 @@
 #include "cons_cell.h"
 
 void _print_dotted_pair(char *buffer, cons_cell *cell);
-void _print_list(char *buffer, cons_cell *cell);
+char *_print_list(char *buffer, cons_cell *cell);
 char *_print_value(char *buffer, void *ptr, cell_flags flags);
 
 void _print_dotted_pair(char *buffer, cons_cell *cell)
@@ -13,7 +13,7 @@ void _print_dotted_pair(char *buffer, cons_cell *cell)
   buffer += sprintf(buffer - 1, ")");
 }
 
-void _print_list(char *buffer, cons_cell *cell)
+char *_print_list(char *buffer, cons_cell *cell)
 {
   cons_cell *current;
 
@@ -24,7 +24,8 @@ void _print_list(char *buffer, cons_cell *cell)
   }
 
   buffer--;
-  sprintf(buffer, ")");
+  buffer += sprintf(buffer, ")");
+  return buffer;
 }
 
 char *_print_value(char *buffer, void *ptr, cell_flags flags)
@@ -39,6 +40,11 @@ char *_print_value(char *buffer, void *ptr, cell_flags flags)
 
   if (flags & FLAG_IS_SYMBOL) {
     return buffer + sprintf(buffer, "%ls ", (char *)ptr);
+  }
+
+  if (flags & FLAG_IS_POINTER) {
+    buffer = _print_list(buffer, (cons_cell *)ptr);
+    return buffer + sprintf(buffer, " ");
   }
 
   assert(0);
--- tests/cons_cell_tests.c
+++ tests/cons_cell_tests.c
@@ -32,6 +32,26 @@ void _test_print_list()
   free(cell_3);
 }
 
+void _test_print_list_of_lists()
+{
+  long one = 1;
+  long two = 2;
+  long three = 3;
+  long four = 4;
+
+  cons_cell *cell_6 = new_cell(&four, FLAG_IS_INT, NULL, FLAG_IS_POINTER);
+  cons_cell *cell_5 = new_cell(&three, FLAG_IS_INT, cell_6, FLAG_IS_POINTER);
+
+  cons_cell *cell_4 = new_cell(&two, FLAG_IS_INT, NULL, FLAG_IS_POINTER);
+  cons_cell *cell_3 = new_cell(&one, FLAG_IS_INT, cell_4, FLAG_IS_POINTER);
+
+  cons_cell *cell_2 = new_cell(cell_5, FLAG_IS_POINTER, NULL, FLAG_IS_POINTER);
+  cons_cell *cell_1 = new_cell(cell_3, FLAG_IS_POINTER, cell_2, FLAG_IS_POINTER);
+
+  print_cell(buffer, cell_1);
+  assert(strncmp(buffer, "((1 2) (3 4))", BUFFER_SIZE) == 0);
+}
+
 void run_cons_cell_tests()
 {
   /* TODO: buffer overrun */
@@ -39,4 +59,5 @@ void run_cons_cell_tests()
 
   RUN_TEST(_test_print_dotted_pair);
   RUN_TEST(_test_print_list);
+  RUN_TEST(_test_print_list_of_lists);
 }
